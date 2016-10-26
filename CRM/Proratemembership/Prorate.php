0eaa4944cf9fa0f7e1875d7c549b37bff5586c4f
Alice Frumin <alice@aghstrategies.com>
2016-10-26 14:27:15 -0400

    adding rollOver date to object

--- CRM/Proratemembership/Prorate.php
+++ CRM/Proratemembership/Prorate.php
@@ -45,6 +45,7 @@ class CRM_Proratemembership_Prorate {
       $rolloverDateFormatted = $endYear . "-" . $rolloverDate[0] . "-" . $rolloverDate[1];
       $rolloverDate = date_create_from_format('Y-m-d', "$rolloverDateFormatted");
       $rolloverDate = date_timestamp_get($rolloverDate);
+      $this->rollOver = $rolloverDate;
       $fixedStartDate = $startYear . '-' . $fixedStartMonthDay[0] . '-' . $fixedStartMonthDay[1];
       $fixedStartDate = date_timestamp_get(date_create_from_format('Y-m-d', "$fixedStartDate"));
       $this->fraction = ($rolloverDate - $today) / ($rolloverDate - $fixedStartDate);
